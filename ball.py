56664e6f18c74bed4c466ea4ef868afc2c666379
eldubs <91855064+lyonelw@users.noreply.github.com>
2025-03-18 15:29:50 -0700

    updated physics again

--- ball.py
+++ ball.py
@@ -9,8 +9,8 @@ monitor_height = user32.GetSystemMetrics(1)
 window_x = 0
 window_y = 0
 blaheight = (0, 0)
-grav = 0.1
-loss = 0.9
+grav = 1
+loss = 0.8
 bx = 0
 by = 0
 wdx = 0
@@ -36,8 +36,8 @@ class BouncingBall(arcade.Window):
         self.ball_screen_x = window_to_screenx(width // 2)
         self.ball_screen_y = window_to_screeny(height // 2, blaheight[1])
         self.ball_radius = 20
-        self.dx = -2
-        self.dy = -2
+        self.dx = -1
+        self.dy = -1
         self.set_update_rate(1/60)
 
     def on_draw(self):
@@ -49,8 +49,9 @@ class BouncingBall(arcade.Window):
         global window_x, window_y, blaheight
         global bx, by
         global wdx, wdy, hist
+        #FIX GRAVITY SO YOU DONT FUCKING SUCK
+        if self.dy < 0.1 or self.dy > -0.1: self.dy += grav
 
-        self.dy += grav
         self.ball_screen_x += self.dx
         self.ball_screen_y += self.dy
         
