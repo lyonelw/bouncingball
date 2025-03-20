5edb1bd2ba41a4d6a1b18f684cfd454829fd2b94
eldubs <91855064+lyonelw@users.noreply.github.com>
2025-03-20 14:43:29 -0700

    added scaling collision as well as position

--- ball.py
+++ ball.py
@@ -1,5 +1,7 @@
 import arcade
 import ctypes
+import time
+
 
 width = 800
 height = 600
@@ -9,13 +11,18 @@ monitor_height = user32.GetSystemMetrics(1)
 window_x = 0
 window_y = 0
 blaheight = (0, 0)
-grav = 0.1
+grav = 0.2
 loss = 0.8
 bx = 0
 by = 0
 wdx = 0
 wdy = 0
+wdsx = 0
+wdsy = 0
+maxspeed = 5
 hist = [(0,0),(0,0),(0,0),(0,0),(0,0)]
+histsize = [(0,0),(0,0),(0,0),(0,0),(0,0)]
+collisions = 0
 
 def window_to_screenx(x):
     return (monitor_width/2 - x)
@@ -35,7 +42,7 @@ class BouncingBall(arcade.Window):
         super().__init__(width, height, "ball", resizable=True)
         self.ball_screen_x = window_to_screenx(width // 2)
         self.ball_screen_y = window_to_screeny(height // 2, blaheight[1])
-        self.ball_radius = 20
+        self.ball_radius = 25
         self.dx = -1
         self.dy = -1
         self.set_update_rate(1/144)
@@ -47,54 +54,74 @@ class BouncingBall(arcade.Window):
     def on_update(self, _delta_time):
         global window_x, window_y, blaheight
         global bx, by
-        global wdx, wdy, hist
-        print(f"x: {self.dx} || y:{self.dy}")
+        global wdx, wdy, hist, histsize
+        global wdsx, wdsy, maxspeed, collisions
+        #print(f"x: {self.dx} || y:{self.dy}")
         #FIX GRAVITY SO YOU DONT FUCKING SUCK
         self.dy += grav
-
         self.ball_screen_x += self.dx
         self.ball_screen_y += self.dy
-        
+        print(collisions)
         blaheight = self.get_size()
         window_x, window_y = self.get_location()
         bx, by = window_to_screenx(window_x + self.ball_screen_x), window_to_screeny(window_y - self.ball_screen_y, blaheight[1])
         hist = scoot(hist, (window_x, window_y))
+        histsize = scoot(histsize, blaheight)
+        wdsx, wdsy = avglist(histsize)
         wdx, wdy = avglist(hist)
+        #print(f"changex: {wdsx}, changey: {wdsy}")
         twidth, theight = blaheight
         if bx > twidth - self.ball_radius:
+            collisions+=1
+            self.dy *= loss
             self.dx *= loss
+
+            self.dy *= 1
+            self.dx *= 1
+
             self.dx = self.dx * -1
+            
             self.dx -= (wdx / 2)
-            #self.dy += (wdy / 4)
+            self.dx -= (wdsx / 2)
         if bx < 0 + self.ball_radius:
+            collisions+=1
+
+            self.dy *= loss
             self.dx *= loss
+
+            self.dy *= 1
+            self.dx *= 1
+            
             self.dx = self.dx * -1
+            
             self.dx -= (wdx / 2)
-            #self.dy += (wdy / 4)
+            self.dx -= (wdsx / 2)
         if by > theight - self.ball_radius:
-            self.dy = self.dy * -1
+            collisions+=1
+
             self.dy *= loss
+            self.dx *= loss
+
+            self.dy *= 1
+            self.dx *= 1
+            
+            self.dy = self.dy * -1
+            
             self.dy += (wdy / 2)
-            #self.dx += (wdx / 4)
+            self.dy -= (wdsy / 2)
         if by < 0 + self.ball_radius:
-            self.dy = self.dy * -1
+            collisions+=1
+
             self.dy *= loss
+            self.dx *= loss
+
+            self.dy *= 1
+            self.dx *= 1
+            
+            self.dy = self.dy * -1
+            
             self.dy += (wdy / 2)
-            #self.dx += (wdx / 4)
-        #stoppages
-        """if bx > twidth + self.ball_radius * 2:
-            self.dx = 0
-            self.dy = 0
-        if bx < 0 - self.ball_radius * 2:
-            self.dx = 0
-            self.dy = 0
-        if by > theight + self.ball_radius * 2:
-            self.dx = 0
-            self.dy = 0
-        if by < 0 - self.ball_radius * 2:
-            self.dx = 0
-            self.dy = 0"
-            """
+            self.dy += (wdsy / 2)
 if __name__ == "__main__":
     game = BouncingBall()
     arcade.run()
